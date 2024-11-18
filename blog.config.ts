bc65caf71d0ba24dff6a1debcc37db01e2a77365
istarry123 <zjy442592754@gmail.com>
2024-11-18 16:47:42 +0800

    2

--- blog.config.ts
+++ blog.config.ts
@@ -19,10 +19,10 @@ type Config = {
 };
 
 export const config: Config = {
-    title: '幻非',
-    description: '幻非的个人博客，记录一些技术或者想法',
-    keywords: ['blog', '博客', '幻非', '技术', '生活', 'huanfei'],
-    url: 'https://www.huanfei.top',
+    title: 'Istarry',
+    description: 'Istarry的个人博客，记录一些技术或者想法',
+    keywords: ['blog', '博客', '技术', '生活', 'Istarry'],
+    url: 'https://web.istarry.top',
     language: 'zh-CN',
     comment: {
         repo: 'huanfe1/blog',
